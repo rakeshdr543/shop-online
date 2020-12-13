06f4a0c777f9a0533f6cb7d49916eabd32c1a1a5
rakeshdr543 <rakeshdr543@gmail.com>
2020-12-13 14:11:01 +0530

    cart added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '
 import { ShoppingCart } from '@material-ui/icons';
 import useStyles from './styles';
 import logo from '../../assets/commerce.png';
-const Navbar = () => {
+const Navbar = ({totalItems}) => {
     const classes=useStyles();
     return (
         <>
@@ -16,7 +16,7 @@ const Navbar = () => {
                   <div className={classes.grow}/>
                       <div className={classes.button}>
                           <IconButton aria-label='Show Cart items' color='inherit'>
-                              <Badge badgeContent={2} color='secondary'>
+                              <Badge badgeContent={totalItems} color='secondary'>
                                   <ShoppingCart/>
                               </Badge>
                           </IconButton>
--- src/components/products/product/Product.jsx
+++ src/components/products/product/Product.jsx
@@ -3,9 +3,9 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
-const Product = ({product}) => {
+const Product = ({product,onAddToCart}) => {
     const classes=useStyles();
-
+    const handleAddToCart = () => onAddToCart(product.id, 1);
     return (
         <Card className={classes.root} >
             <CardMedia className={classes.media} image={product.media.source} title={Product.name} />
@@ -21,7 +21,7 @@ const Product = ({product}) => {
                 <Typography dangerouslySetInnerHTML={{__html:product.description}} variant='body' color='textSecondary' />
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label='Added to Cart'>
+                <IconButton aria-label='Added to Cart' onClick={handleAddToCart}>
                     <AddShoppingCart />
                 </IconButton>
             </CardActions>
